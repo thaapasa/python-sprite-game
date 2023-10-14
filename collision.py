5a158ef5cac085a3835af88026c1e50becff6063
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2023-10-14 22:40:58 +0300

    Finetune

--- collision.py
+++ collision.py
@@ -3,17 +3,27 @@ import pygame.sprite
 
 
 def find_mtv(target: pygame.Rect, o: pygame.Rect, velocity: pygame.Vector2):
+    """Finds minimum translation vector that moves target backwards along
+    velocity so it doesn't collide with o anymore"""
     coll_box = target.clip(o)
 
+    # Collision amount, per axis
     cx = coll_box.width
     cy = coll_box.height
     if cx == 0 and cy == 0:
         return None
 
+    # Adjust direction
     if target.centerx < o.centerx:
         cx = -cx
     if target.centery < o.centery:
         cy = -cy
+
+    # Make very small adjustments simply
+    if abs(cx) <= 1:
+        return (cx, 0)
+    if abs(cy) <= 1:
+        return (0, cy)
 
     # pushback
     pb = velocity.normalize() * -1
